2e6395559df61f6f5ad7cdf69b24fd13f8ef9d41
Sophia Allen <smlelevich@gmail.com>
2017-01-24 11:32:59 -0800

    answered first two qs

---
+++ subqueries.sql
@@ -0,0 +1,22 @@
+-- 1. Return the employee key, last name and first name, position name and hourly rate 
+-- for those employees receiving the maximum pay rate
+
+Select em.EmployeeKey, EmployeeFirstName, EmployeeLastName, PositionName, EmployeeHourlyPayRate
+from Employee em 
+inner join EmployeePosition ep
+on em.EmployeeKey = ep.EmployeeKey
+inner join Position p 
+on ep.PositionKey = p.PositionKey
+where EmployeeHourlyPayRate in 
+(Select Max(EmployeeHourlyPayRate) from EmployeePosition)
+
+
+--2. Use only subqueries to do this. Return the key, last name and
+-- first name of every employee who has the position name “manager.”
+
+Select EmployeeKey, EmployeeLastName, EmployeeFirstName 
+from Employee 
+Where EmployeeKey in (
+Select EmployeeKey from EmployeePosition 
+where PositionKey = (
+Select PositionKey from Position where PositionName = 'Manager'))
