08f5b070a7f10730387fe756210299b00cfc4a44
Sophia Allen <smlelevich@gmail.com>
2017-03-16 11:30:43 -0700

    Update admin_commands.sql

--- hw/admin_commands.sql
+++ hw/admin_commands.sql
@@ -1,3 +1,18 @@
+--Create a Schema called "ManagementSchema."
+create schema ManagementSchema
+
+go 
+
+--Create a view owned by the schema that shows the annual ridership.
+create view ManagementSchema.RidershipInfo
+Select Year(BusScheduleAssignmentDate) as 'Year', sum(Riders) as 'Annual Total', avg(Riders) as 'Annual Average',
+(Select sum(Riders) from Ridership) as 'Total',
+Cast((Sum(Riders) * 1.0)/(Select sum(Riders) from Ridership) * 100 as decimal(10,2))  as 'Percent'
+from Ridership inner join BusScheduleAssignment bsa
+on Ridership.BusScheduleAssigmentKey = bsa.BusScheduleAssignmentKey
+group by Year(bsa.BusScheduleAssignmentDate)
+--Note: I re-used the query from my Subqueries homework assignment. 
+
 go 
 
 --Create a view owned by the schema that shows the employee information including their position and pay rate.
@@ -20,8 +35,6 @@ Create role ManagementRole
 Grant Select on schema::ManagementSchema to ManagementRole
 Grant exec on dbo.usp_RegisterNewEmployee to ManagementRole
 
-
-
 --Create a new login for one of the employees who holds the manager position.
 Create login ManagersLogin with password = 'P@ssw0rd1'
 
@@ -32,6 +45,15 @@ Create user ManagerLogin for login ManagersLogin
 Alter role ManagementRole add member ManagerLogin
 
 --Login to the database as the new User, (Remember that SQL server authentication must be enabled for this to work.)
---Run the query to view the annual ridership. Does it work?
+--Run the query to view the annual ridership. Does it work? 
+-- >> Yes
+
 --Try to select from the table Employees. Can you do it?
+-- >> No, "SELECT permission denied"
+
 --Back up the database MetroAlt.
+backup database MetroAlt to disk='C:\Backups\MetroAlt.bak'
+--Result: 
+-->> Processed 3080 pages for database 'MetroAlt', file 'MetroAlt' on file 1.
+-->> Processed 2 pages for database 'MetroAlt', file 'MetroAlt_log' on file 1.
+-->> BACKUP DATABASE successfully processed 3082 pages in 0.854 seconds (28.187 MB/sec).
