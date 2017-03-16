acca5d5fb4a85885c6328e094e3546c56cb65a52
Sophia Allen <smlelevich@gmail.com>
2017-03-16 11:32:20 -0700

    Update admin_commands.sql

--- hw/admin_commands.sql
+++ hw/admin_commands.sql
@@ -1,9 +1,9 @@
---Create a Schema called "ManagementSchema."
+--1. Create a Schema called "ManagementSchema."
 create schema ManagementSchema
 
 go 
 
---Create a view owned by the schema that shows the annual ridership.
+--2. Create a view owned by the schema that shows the annual ridership.
 create view ManagementSchema.RidershipInfo
 Select Year(BusScheduleAssignmentDate) as 'Year', sum(Riders) as 'Annual Total', avg(Riders) as 'Annual Average',
 (Select sum(Riders) from Ridership) as 'Total',
@@ -28,30 +28,30 @@ on ep.PositionKey = p.PositionKey
 go
 
 
---Create a role ManagementRole.
+--3. Create a role ManagementRole.
 Create role ManagementRole
 
---Give the ManagementRole select permissions on the ManagementSchema and Exec permissions on the new employee stored procedure we created earlier.
+--4. Give the ManagementRole select permissions on the ManagementSchema and Exec permissions on the new employee stored procedure we created earlier.
 Grant Select on schema::ManagementSchema to ManagementRole
 Grant exec on dbo.usp_RegisterNewEmployee to ManagementRole
 
---Create a new login for one of the employees who holds the manager position.
+--5. Create a new login for one of the employees who holds the manager position.
 Create login ManagersLogin with password = 'P@ssw0rd1'
 
---Create a new user for that login.
+--6. Create a new user for that login.
 Create user ManagerLogin for login ManagersLogin
 
---Add that user to the Role.
+--7. Add that user to the Role.
 Alter role ManagementRole add member ManagerLogin
 
---Login to the database as the new User, (Remember that SQL server authentication must be enabled for this to work.)
---Run the query to view the annual ridership. Does it work? 
+--8. Login to the database as the new User, (Remember that SQL server authentication must be enabled for this to work.)
+--9. Run the query to view the annual ridership. Does it work? 
 -- >> Yes
 
---Try to select from the table Employees. Can you do it?
+--10. Try to select from the table Employees. Can you do it?
 -- >> No, "SELECT permission denied"
 
---Back up the database MetroAlt.
+--11. Back up the database MetroAlt.
 backup database MetroAlt to disk='C:\Backups\MetroAlt.bak'
 --Result: 
 -->> Processed 3080 pages for database 'MetroAlt', file 'MetroAlt' on file 1.
